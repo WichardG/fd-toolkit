9c033c33568917ea4e4941080c7c3339549bed01
WichardG <wichardhg@gmail.com>
2023-03-11 20:47:06 +0100

    promp question logic

--- src/components/Prompt/QuestionPrompts.vue
+++ src/components/Prompt/QuestionPrompts.vue
@@ -7,6 +7,19 @@
 </template>
 
 <script setup lang="ts">
+import type { promptItem } from '@/pages/Prompt/logic';
+import { onMounted } from 'vue';
+
+    const emit = defineEmits<{
+        (event: 'nextQuestion', id: number): void
+    }>()
+
+    const props = defineProps<{items: promptItem[]}>()
+
+    const getNextPromptItemByID = (id: number) => props.items.find(item => item.id === id);
+
+    onMounted(()=> console.log(props.items))
+
 </script>
 
 <style scoped>
