1df805e02b1b5c06b0d62fda849927e66f74ac51
WichardG <wichardhg@gmail.com>
2023-03-11 19:45:14 +0100

    QuestionPrompt component

--- src/pages/Prompt/PromptOverview.vue
+++ src/pages/Prompt/PromptOverview.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h1>This is an about page</h1>
+        <QuestionPromptsVue/>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type QuestionPromptsVue from '@/components/Prompt/QuestionPrompts.vue';
+
+</script>
