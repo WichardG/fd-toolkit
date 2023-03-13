0b31e87e973153b265b0fc739ac641cb8b9ba7ee
YohanStoffers <yohanstoffers18@gmail.com>
2023-03-13 21:04:03 +0100

    methodes logic

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,15 +13,36 @@ const router = createRouter({
             name: 'prompts',
             component: () => import('../pages/Prompt/PromptOverview.vue'),
         },
+        // methodes page
         {
-            path: '/Methodes/Foo',
-            name: 'methodes-foo',
+            path: '/Methodes/backcasting',
+            name: 'methodes-backcasting',
             component: () => import('../pages/methodes/backcasting.vue'),
         },
         {
-            path: '/Methodes/Bar',
-            name: 'methodes-bar',
+            path: '/Methodes/roadmapping',
+            name: 'methodes-roadmapping',
             component: () => import('../pages/methodes/roadmapping.vue'),
+        },
+        {
+            path: '/Methodes/sciencefictionPrototyping',
+            name: 'methodes-sciencefictionprototyping',
+            component: () => import('../pages/methodes/sciencefictionPrototyping.vue'),
+        },
+        {
+            path: '/Methodes/speculatiefOntwerp',
+            name: 'methodes-speculatiefontwerp',
+            component: () => import('../pages/methodes/speculatiefOntwerp.vue'),
+        },
+        {
+            path: '/Methodes/toekomstscenario',
+            name: 'methodes-toekomstscenario',
+            component: () => import('../pages/methodes/toekomstscenario.vue'),
+        },
+        {
+            path: '/Methodes/trendanalyse',
+            name: 'methodes-trendanalyse',
+            component: () => import('../pages/methodes/trendanalyse.vue'),
         },
     ],
 });
